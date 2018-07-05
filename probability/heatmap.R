7211d68340328a8ae2e0a45b57910adbd5107019
Jinjing <jillzhoujinjing@gmail.com>
2018-07-05 15:04:47 -0700

    heatmap color adjusted

--- probability/heatmap.R
+++ probability/heatmap.R
@@ -2,7 +2,7 @@ args = commandArgs(trailingOnly=TRUE)
 datafile = args[1]
 
 Data=read.csv(datafile,header=F,sep=" ")
-colnames(Data) <- c("N_GWAS","N_Rep","lambda","power","both_positive")
+colnames(Data) <- c("N_GWAS","N_Rep","lambda","Pr_true_pos_GWAS","Pr_true_pos_GWAS_Rep")
 
 #installing packages
 #install.packages('ggplot2')
@@ -12,8 +12,10 @@ library(ggplot2)
 library(reshape2)
 
 for( i in 1:5){
-  temp_graph <- ggplot(subset(Data,lambda==i*0.01),aes(x=N_GWAS,y=N_Rep))+geom_tile(aes(fill = power))
+  power_g <- ggplot(subset(Data,lambda==i*0.01),aes(x=N_GWAS,y=N_Rep))+geom_tile(aes(fill = Pr_true_pos_GWAS))
+  power_g+scale_fill_continuous(limits=c(0, 1), breaks=seq(0,1,by=0.1), low="white",high="steelblue")
   ggsave(paste("power-",as.character(i*0.01),".jpg",sep=""))
-  graph_temp <- ggplot(subset(Data,lambda==i*0.01),aes(x=N_GWAS,y=N_Rep))+geom_tile(aes(fill = both_positive))
+  both_g <- ggplot(subset(Data,lambda==i*0.01),aes(x=N_GWAS,y=N_Rep))+geom_tile(aes(fill = Pr_true_pos_GWAS_Rep))
+  both_g + scale_fill_continuous(limits=c(0, 1), breaks=seq(0,1,by=0.1), low="white",high="steelblue")
   ggsave(filename=paste("prb_btp-",as.character(i*0.01),".jpg",sep=""))
 }
