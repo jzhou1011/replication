37265376ab6e55895b3e767accfdead4c0664bb7
Jinjing <jillzhoujinjing@gmail.com>
2018-07-16 11:26:22 -0700

    added lines with varying sample sizes

--- probability/PowerVSLamda.R
+++ probability/PowerVSLamda.R
@@ -6,21 +6,50 @@ library(ggplot2)
 #   return(power)
 # }
 
-power <- function(lamda) {
-  alphaStar = .00000001
+power_1k <- function(lamda) {
+  alphaStar = .0000001
+  N=1000
+  power <- pnorm(qnorm(alphaStar/2)+lamda*sqrt(N))+1-pnorm(-qnorm(alphaStar/2)+lamda*sqrt(N))
+  return(power)
+}
+
+powerSquared_1k <- function(lamda){
+  power_1k(lamda)*power_1k(lamda)
+}
+
+
+power_10k <- function(lamda) {
+  alphaStar = .0000001
   N=10000
   power <- pnorm(qnorm(alphaStar/2)+lamda*sqrt(N))+1-pnorm(-qnorm(alphaStar/2)+lamda*sqrt(N))
   return(power)
 }
 
-powerSquared <- function(lamda){
-  power(lamda)*power(lamda)
-  }
+powerSquared_10k <- function(lamda){
+  power_10k(lamda)*power_10k(lamda)
+}
+
+power_100k <- function(lamda) {
+  alphaStar = .0000001
+  N=100000
+  power <- pnorm(qnorm(alphaStar/2)+lamda*sqrt(N))+1-pnorm(-qnorm(alphaStar/2)+lamda*sqrt(N))
+  return(power)
+}
+
+powerSquared_100k <- function(lamda){
+  power_100k(lamda)*power_100k(lamda)
+}
+
 
 plot<-ggplot(data.frame(x=c(0,0.11)), aes(x=x))+xlab("Lamda")+ylab("Probability")+
-    stat_function(fun=powerSquared, geom="line", aes(colour="GWAS=1, Rep=1"))+
-    stat_function(fun=power, geom="line", aes(colour="The Power"))+scale_colour_manual(values=c("blue", "red"))+
-    ggtitle("                  Probability of the power and two true positives")
+    stat_function(fun=powerSquared_1k, geom="line", aes(colour="Replication, N=1k"))+
+    stat_function(fun=power_1k, geom="line", aes(colour="Power, N=1k"))+
+    stat_function(fun=powerSquared_10k, geom="line", aes(colour="Replication, N=10k"))+
+    stat_function(fun=power_10k, geom="line", aes(colour="Power, N=10k"))+
+    stat_function(fun=powerSquared_100k, geom="line", aes(colour="Replication, N=100k"))+
+    stat_function(fun=power_100k, geom="line", aes(colour="Power, N=100k"))+
+    ggtitle("         Power and the probability of replication")+
+    xlim(0,0.3)
 
 ggsave(filename = "Power&BothReplication_ProbabilityCurve.jpg")
 
