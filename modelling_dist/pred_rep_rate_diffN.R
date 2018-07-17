b9ac37e5d1e25d85bd8982985c16bf5cbc2d1077
Sarah Faller <sarahfaller@Sarahs-MacBook-Air-3.local>
2018-07-17 19:18:26 -0400

    different sample sizesfixed

--- modelling_dist/pred_rep_rate_diffN.R
+++ modelling_dist/pred_rep_rate_diffN.R
@@ -17,8 +17,8 @@ zScore=5.2
 calculate_pcondtional<-function(s1,sampleS1, sampleS2){
   sd_S1<-sqrt(sampleS1*sigma^2+1)
   sd_S2<-sqrt(sampleS2*sigma^2+1)
-  mean<-(sqrt(sampleS1*sampleS2)*sigma^2*s1)/(sd_S2^2)
-  var2<-1+((sampleS1*sigma^2)/(sd_S2)^2)
+  mean<-(sqrt(sampleS1*sampleS2)*sigma^2*s1)/(sd_S1^2)
+  var2<-1+((sampleS2*sigma^2)/(sd_S1)^2)
   p<- (1-pnorm(zScore, mean, sqrt(var2)))+pnorm(-zScore, mean, sqrt(var2))
   return(p)
 }
@@ -40,6 +40,8 @@ table1$Ps2<-calculate_pcondtional(table1$S1, N_1, table1$N2[1])
 P_S1_1<-ggplot(data=table1, mapping=aes(x=S1))+
   stat_function(fun=calculate_pcondtional, args=list(sampleS1=N_1, sampleS2=N_1*0.5))
 
+P_S1_1
+
 
 table2<-as.data.frame(matrix(nrow=N, ncol=4))
 colnames(table2)=c("N1", "N2", "S1", "Ps2")
@@ -51,6 +53,7 @@ table2$Ps2<-calculate_pcondtional(table2$S1, N_1, table2$N2[1])
 P_S1_2<-ggplot(data=table2, mapping=aes(x=S1))+
   stat_function(fun=calculate_pcondtional, args=list(sampleS1=N_1, sampleS2=N_1))
 
+P_S1_2
 
 table3<-as.data.frame(matrix(nrow=N, ncol=4))
 colnames(table3)=c("N1", "N2", "S1", "Ps2")
@@ -62,6 +65,8 @@ table3$Ps2<-calculate_pcondtional(table3$S1, N_1, table3$N2[1])
 P_S1_3<-ggplot(data=table3, mapping=aes(x=S1))+
   stat_function(fun=calculate_pcondtional, args=list(sampleS1=N_1, sampleS2=N_1*2))
 
+P_S1_3
+
 
 table4<-as.data.frame(matrix(nrow=N, ncol=4))
 colnames(table4)=c("N1", "N2", "S1", "Ps2")
@@ -69,6 +74,12 @@ table4$N1<-rep(N_1, N)
 table4$N2<-rep(N_1*5, N)
 table4$S1<-s1_dist
 table4$Ps2<-calculate_pcondtional(table4$S1, N_1, table4$N2[1])
+
+P_S1_4<-ggplot(data=table4, mapping=aes(x=S1))+
+  stat_function(fun=calculate_pcondtional, args=list(sampleS1=N_1, sampleS2=N_1*5))
+
+
+P_S1_4
 
 
 
