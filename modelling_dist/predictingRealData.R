7dc5824028687dfaf339d7e585795f2488a6211c
Sarah Faller <sarahfaller@Sarahs-MacBook-Air-3.local>
2018-07-17 19:49:01 -0400

    different sample sizesfixed

--- modelling_dist/predictingRealData.R
+++ modelling_dist/predictingRealData.R
@@ -27,8 +27,8 @@ var=data$trait.var[1]
 sigma=sqrt(var)
 threshold=data$p.thresh[1]
 zScore=qnorm(threshold, lower.tail = FALSE)
-sampleSizeS1=data$n.disc
-sampleSizeS2=data$n.rep
+sampleSizeS1=1
+sampleSizeS2=data$n.rep/data$n.disc
 
 
 #calculating condtional with different sample sizes.
@@ -44,12 +44,11 @@ sampleSizeS2=data$n.rep
 calculate_pcondtional<-function(s1,sampleS1, sampleS2){
   sd_S1<-sqrt(sampleS1*sigma^2+1)
   sd_S2<-sqrt(sampleS2*sigma^2+1)
-  mean<-(sqrt(sampleS1*sampleS2)*sigma^2*s1)/(sd_S2^2)
-  var2<-1+((sampleS1*sigma^2)/(sd_S2)^2)
+  mean<-(sqrt(sampleS1*sampleS2)*sigma^2*s1)/(sd_S1^2)
+  var2<-1+((sampleS2*sigma^2)/(sd_S1)^2)
   p<- (1-pnorm(zScore, mean, sqrt(var2)))+pnorm(-zScore, mean, sqrt(var2))
   return(p)
 }
-
 
 #observed replication rate 
 s1_sig<-nrow(filter(results.data, s1>zScore|s1<(-zScore)))
@@ -58,5 +57,5 @@ repRate=s2_sig_givens1/s1_sig
 
 s1_sig_vector <-filter(results.data, s1>zScore|s1<(-zScore))$s1
 #theo_rep2 <- sum(calculate_pcondtional(s1_sig_vector, sampleSizeS1, sampleSizeS2))/N
-theo_rep2 <- sum(calculate_pcondtional(s1_sig_vector))/N
+theo_rep2 <- sum(calculate_pcondtional(s1_sig_vector,sampleSizeS1, sampleSizeS2))/N
 
