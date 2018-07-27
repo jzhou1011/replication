16fe11e739fa1e862242d9a955a5b73fc9125b80
Sarah Faller <sarahfaller@Sarahs-MacBook-Air-3.local>
2018-07-27 13:47:33 -0400

    checing var 1 with simulated data

--- predicting real data/isVar1.R
+++ predicting real data/isVar1.R
@@ -3,10 +3,13 @@
 #library(tidyverse)
 #library(dplyr)
 
+#read in data
+filename<-"./files/1_25282103_data_upbuilt_filtered_upbuilt.csv" #good 
+filename<-"./files/3_23118974_data_upbuilt_filtered_upbuilt.csv"
 
-#reading in data
-args = commandArgs(trailingOnly=TRUE)
-filename<-args[1]
+#reading in data through command line 
+# args = commandArgs(trailingOnly=TRUE)
+# filename<-args[1]
 data<-read.csv(filename, sep=",")
 
 
@@ -14,8 +17,6 @@ z_score <- qnorm(0.025,lower.tail =FALSE)
 M=nrow(data)
 var=data$trait.var[1]
 sigma=sqrt(var)
-threshold=data$p.thresh[1]
-
 
 #calculating averages for scaling 
 averages1=mean(data$n.disc)
@@ -23,12 +24,12 @@ averages2=mean(data$n.rep)
 
 #find test statsitic with scaling
 results.data<-data.frame(data$beta.disc, data$se.disc)
-results.data$s1<-((data$beta.disc)/(data$se.disc))*(sqrt(averages2))
-results.data$s2<-((data$beta.rep)/(data$se.rep))*(sqrt(averages1))
+results.data$s1<-((data$beta.disc)/(data$se.disc))
+results.data$s2<-((data$beta.rep)/(data$se.rep))
 
-temp<-results.data$s1-results.data$s2
-temp<-temp/(sqrt(averages1+averages2))
-act_var<-var(temp)
+temp<-((results.data$s1)*(sqrt(averages2)))-(results.data$s2*(sqrt(averages1)))
+temp<-temp
+act_var<-var(temp)/(averages1+averages2)
 
 #act_var<-act_var/(sqrt(averages2+averages1))
 act_var_f <- formatC(act_var, width = 4, format="fg")
---
+++ predicting real data/simulatedDataVar1.R
@@ -0,0 +1,56 @@
+#predicting whether s2 replicates given s1. Different Sample Size 
+
+library(tidyverse)
+library(MASS)
+library(ggplot2)
+library(dplyr)
+library(plyr)
+library(reshape2)
+
+#simulating data 
+var<-13.807
+sigma<-sqrt(var)
+N=1000000
+N_1=4627
+N_2=9316
+#N_1=10000
+z_score=5.2
+
+
+calculate_pcondtional<-function(s1,sampleS1, sampleS2){
+  sd_S1<-sqrt(sampleS1*sigma^2+1)
+  sd_S2<-sqrt(sampleS2*sigma^2+1)
+  mean<-(sqrt(sampleS1)*sqrt(sampleS2)*sigma^2*s1)/(sd_S1^2)
+  var2<-1+((sampleS2*sigma^2)/(sd_S1)^2)
+  p <- s1
+  for (i in 1:NROW(s1)){
+    if (s1[i]>0){
+      p[i]<-(1-pnorm(z_score, mean[i], sqrt(var2)))
+    }
+    else{
+      p[i]<-pnorm(-z_score, mean[i], sqrt(var2))
+    }
+  }
+  return(p)
+}
+
+lamda <- rnorm(n=N, mean=0, sd=sigma)
+
+s1_dist<-matrix(nrow=N, ncol=1)
+s2_dist1<-matrix(nrow=N, ncol=1)
+
+for (i in 1:N){
+  s1_dist[i]<-rnorm(n=1,mean=lamda[i]*sqrt(N_1),sd=1)
+  s2_dist1[i]<-rnorm(n=1,mean=lamda[i]*sqrt(N_2),sd=1)
+}
+
+s1_dist<-as.vector(s1_dist)
+s2_dist1<-as.vector(s2_dist1)
+
+
+temp1<-((s1_dist)*(sqrt(N_2)))-((s2_dist1)*(sqrt(N_1)))
+temp1<-temp1/(sqrt(N_1+N_2))
+act_var<-var(temp1)
+
+
+
