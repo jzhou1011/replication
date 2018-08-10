61803a8513ce07a6bddcf1a56c097e4263877698
Jinjing <jillzhoujinjing@gmail.com>
2018-08-10 15:54:52 -0700

    script update

--- predicting real data/table.sh
+++ predicting real data/table.sh
@@ -4,19 +4,18 @@
 IFS=$'\n' read -d '' -r -a lines < ./input.txt
 length=${#lines[@]}
 echo ${length}
-echo "filename      M   obs  pred  var   est_sig" > output.txt
+echo "filename      M   obs  pred_wc   pred_c   var_g   var_c1  var_c2 " > output.txt
 
 counter=0
 while [ $counter -lt $length ]
 do
 	name=${lines[$counter]:8:10}
 	#echo "${lines[$counter]}" | cut -d'_' -f 2 >> output.txt
+	echo "......Running the ${counter}th file......"
 	echo -n $name >> output.txt
 	echo -n " " >> output.txt
-	Rscript predictingRealData.R "${lines[$counter]}" >> output.txt
-	Rscript isVar1.R "${lines[$counter]}" >> output.txt
-	echo -n " " >> output.txt
-	Rscript MLEvar.R "${lines[$counter]}" >> output.txt
+	Rscript predictingRealData.R "${lines[$counter]}" >> output.txt 2> /dev/null
+	Rscript RealDataFinal_cor.R "${lines[$counter]}" >> output.txt 2> /dev/null
 	((counter++))
 done
 
